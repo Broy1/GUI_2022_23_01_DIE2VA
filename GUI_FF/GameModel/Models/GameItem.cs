235c82af62b684a522b6ec718362544280fb8734
Roland Bogya <roy.bogya@gmail.com>
2022-10-11 14:14:35 +0200

    GameItem abstract class created

---
+++ GUI_FF/GameModel/Models/GameItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel.Models
+{
+    // All the objects that can interact are GameItems.
+    public abstract class GameItem
+    {
+        public int X_Row { get; set; }
+        public int Y_Column { get; set; }
+
+        public bool Overlaps(GameItem othergameitem)
+        {
+            return this.X_Row == othergameitem.X_Row && this.Y_Column == othergameitem.Y_Column;
+        }
+    }
+}

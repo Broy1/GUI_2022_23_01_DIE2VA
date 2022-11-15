a54873bf2e568e17ed26463945e5ac99014714ff
Broy1 <74255098+Broy1@users.noreply.github.com>
2022-11-15 11:01:35 +0100

    Fixing folders
    still trying

---
+++ GUI_2022_23_01_DIE2VA/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using GUI_2022_23_01_DIE2VA.Controller;
+using GUI_2022_23_01_DIE2VA.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_2022_23_01_DIE2VA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GameController controller;
+        public MainWindow()
+        {
+            InitializeComponent();
+            GameLogic logic = new GameLogic();
+            display.SetupModel(logic);
+            controller = new GameController(logic);
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            display.Resize(new Size(grid.ActualWidth, grid.ActualHeight));
+            display.InvalidateVisual();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            display.Resize(new Size(grid.ActualWidth, grid.ActualHeight));
+            display.InvalidateVisual();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            controller.KeyPressed(e.Key);
+            display.InvalidateVisual();
+        }
+    }
+}

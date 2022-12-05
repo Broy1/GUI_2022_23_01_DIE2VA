ed54109f93c968300fc283d9764821302d46d2f3
Broy1 <74255098+Broy1@users.noreply.github.com>
2022-12-05 01:19:41 +0100

    Level reset implementation

--- GUI_2022_23_01_DIE2VA/Logic/IGameControl.cs
+++ GUI_2022_23_01_DIE2VA/Logic/IGameControl.cs
@@ -1,9 +1,10 @@
-﻿using static GUI_2022_23_01_DIE2VA.Logic.GameLogic;
+using static GUI_2022_23_01_DIE2VA.Logic.GameLogic;
 
 namespace GUI_2022_23_01_DIE2VA.Logic
 {
     internal interface IGameControl
     {
         void Move(Directions direction);
+        void ResetLevel();
     }
 }
